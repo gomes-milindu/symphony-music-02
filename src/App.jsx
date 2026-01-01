d140839b2ef9d611d6f095bb91d7f42417eecaa1
gomes-milindu <gomesmilindu@gmail.com>
2026-01-01 09:46:17 +0530

    Link navbar with paths

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,11 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<HomePage />} />
+          <Route path="/classes" element={<h1>This is Classes</h1>} />
+          <Route path="/events" element={<h1>This is Events</h1>} />
+          <Route path="/gallery" element={<h1>This is gallery</h1>} />
+          <Route path="/aboutus" element={<h1>This is About Us</h1>} />
+          <Route path="/contactus" element={<h1>This is Contact Us</h1>} />
           
         </Routes>
       </BrowserRouter>

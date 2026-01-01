8f48e2e32287e4a8e8914bf3059a386dda46ba59
dasunithimasha <dasunithimasha25@gmail.com>
2026-01-01 20:41:39 +0530

    fix confilts

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Header from './components/header/headercontent/cont'
-import Student from './components/body/topStudent/student'
-import HomePage from './pages/HomePage'
-import EventPage from './pages/EventPage'
-import EventCard from './components/EventsPageComponents/EventCard'
+
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+
+import Header from "./components/header/headerContent/Header";
+import HomePage from "./pages/HomePage";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <HomePage />
-      {/* <EventPage/> */}
- 
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/classes" element={<h1>This is Classes</h1>} />
+          <Route path="/events" element={<h1>This is Events</h1>} />
+          <Route path="/gallery" element={<h1>This is gallery</h1>} />
+          <Route path="/aboutus" element={<h1>This is About Us</h1>} />
+          <Route path="/contactus" element={<h1>This is Contact Us</h1>} />
+          
+        </Routes>
+      </BrowserRouter>
+      
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,5 +1,5 @@
 import Student from "../components/body/topStudent/student";
-import Header from "../components/header/headercontent/cont";
+import Header from "../components/header/headerContent/Header";
 import OurStory from "../components/body/ourstory/OurStory";
 import ExpertSection from "../components/body/expert/ExpertSection";
 export default function  HomePage(){

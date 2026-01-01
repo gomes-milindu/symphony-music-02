881f5ce554ec9410598d688165658c20ef60d5ca
gomes-milindu <gomesmilindu@gmail.com>
2026-01-02 01:17:27 +0530

    to backup from gomes

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -19,7 +19,7 @@ export default function  HomePage(){
                 <UpComingSlide />
             </div>
             <UpCommingSection />
-            {/* <UpComingSlide /> */}
+            <UpComingSlide />
 
             
                 

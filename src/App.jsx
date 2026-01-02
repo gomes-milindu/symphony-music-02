34ba278ffc8477d8abb1f1427956be22b9776614
dasunithimasha <dasunithimasha25@gmail.com>
2026-01-02 21:16:28 +0530

    Add changes by dasuni

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,28 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 
-import Header from "./components/header/headerContent/Header";
+
 import HomePage from "./pages/HomePage";
+import AboutUs from "./pages/AboutUs";
+import ContactUs from "./pages/ContactUs";
+import Classes from "./pages/Classes";
+import Gallery from "./pages/Gallery";
 
 function App() {
   return (
     <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/classes" element={<Classes />} />
+          <Route path="/events" element={<h1>This is Events</h1>} />
+          <Route path="/gallery" element={<Gallery />} />
+          <Route path="/aboutus" element={<AboutUs />} />
+          <Route path="/contactus" element={<ContactUs />} />
+          
+        </Routes>
+      </BrowserRouter>
       
-      <HomePage />
     </>
   );
 }

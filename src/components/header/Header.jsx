efbb02612a460c79fc87deb0f435dc4811232f37
gomes-milindu <gomesmilindu@gmail.com>
2026-01-02 23:55:30 +0530

    updated homepage with hero section

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,13 +1,13 @@
 import React from "react";
-import Vector7 from "../../../assets/homePage/Vector7.png"; // Under vector
-import MaskgroupLg from "../../../assets/homePage/MaskgroupLg.png";
-import MaskgroupMd from "../../../assets/homePage/MaskgroupMd.png"; // Desktop background
-import Vector8 from "../../../assets/homePage/Vector8.svg"; // Mask overlay
-import Layer1 from "../../../assets/homePage/Layer1.png";
+import Vector7 from "../../assets/homePage/Vector7.png"; // Under vector
+import MaskgroupLg from "../../assets/homePage/MaskgroupLg.png";
+import MaskgroupMd from "../../assets/homePage/MaskgroupMd.png"; // Desktop background
+import Vector8 from "../../assets/homePage/Vector8.png"; // Mask overlay
+import Layer1 from "../../assets/homePage/Layer1.png";
 
-import MaskgroupSm from "../../../assets/homePage/MaskgroupSm.png";
-import Vector9 from "../../../assets/homePage/Vector9.png";
-import Navbar from "../navBar/Navbar";
+import MaskgroupSm from "../../assets/homePage/MaskgroupSm.png";
+import Vector9 from "../../assets/homePage/Vector9.png";
+import Navbar from "../header/navBar/Navbar";
 
 export default function Header() {
   return (
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -5,11 +5,13 @@ import ExpertSection from "../components/body/expert/ExpertSection";
 import Navbar from "../components/header/navBar/Navbar";
 import UpComingSlide from "../section/homepage/UpComingSlide";
 import UpComingSection from "../section/homepage/UpComingSection";
+import Header from "../components/header/Header";
 export default function HomePage() {
   return (
     <>
       <div className="w-full h-full flex flex-col">
-        <Navbar />
+        
+        <Header />
         <OurStory />
         <ExpertSection />
         <Student />

5de2f44092c4d50937de28011824deba11d32e7c
ParindyaHewage <parindyahewage00@gmail.com>
2026-01-03 22:58:07 +0530

    change

--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,10 @@ import './index.css'
 import App from './App.jsx'
 import GridOverlay from './layout/GridOverlay.jsx'
 import { BrowserRouter } from "react-router-dom";
-import PrinciplaMessage from './components/body/principleMessage/principlaMessage.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
       {/* <GridOverlay /> */}
       <App />
-      <PrinciplaMessage/>
   </StrictMode>,
 )
